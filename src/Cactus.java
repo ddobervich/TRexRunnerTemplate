a340d328ce4973bca1d5482c4da3647fe117354a
David Dobervich <david.dobervich@gmail.com>
2018-03-01 09:32:57 -0800

    initial commit

---
+++ src/Cactus.java
@@ -0,0 +1,15 @@
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class Cactus {
+	// Fields
+	
+	// Constructor
+	public Cactus() {
+		
+		
+	}
+
+	
+	
+}
---
+++ src/Game.java
@@ -0,0 +1,16 @@
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class Game extends PApplet {
+	
+	
+	public void setup() {
+		size(800, 400);
+		
+	}
+	
+	public void draw() {
+		
+		
+	}
+}
